01cbc81fcc68ea9e76ff35f25b549fb0d7dc5fa7
David Veselý <dawe.vesely@gmail.com>
2020-10-30 15:05:31 +0100

    Remove gov sources, add basic styles, header, homepage start

--- gov_designsystem_starter_kit/public/js/js-toggle.js
+++
@@ -1,5 +0,0 @@
-/* JS toggle
- * In case is JavaScript enabled remove js-no class and add js-enabled classs to html element.
- *
- */
-document.documentElement.className = document.documentElement.className.replace("js-no","js-ok");
